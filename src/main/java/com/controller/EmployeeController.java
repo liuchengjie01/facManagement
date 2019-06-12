c3fa90b105c73ca552b68cc4aad648e5bfb7ac91
liuchengjie01 <47029970+liuchengjie01@users.noreply.github.com>
2019-06-13 01:54:56 +0800

    add employee

--- src/main/java/com/controller/EmployeeController.java
+++ src/main/java/com/controller/EmployeeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -23,7 +25,90 @@ public class EmployeeController {
         model.addAttribute("list", list);
         return "employee";
     }
-    
+
+    @RequestMapping("/searchEmployee")
+    public String search(Model model, Employee iv){
+        System.out.println("**********"+iv.toString());
+        List<Employee> list = employeeService.queryAllEmployee();
+        Iterator<Employee> it = list.iterator();
+        String attribute = iv.getEmployeeId();
+        if (attribute.equals("")){
+            return "redirect:/Employee/Employee";
+        }
+        String value = iv.getCurDepartmentid();
+        while (it.hasNext()){
+            Employee i = it.next();
+            String a = "";
+            long l = 0;
+            Date d = null;
+            System.out.println("attribute is " + attribute);
+            switch (attribute){
+                case "员工号":
+                    a = i.getEmployeeId();
+                    break;
+                case "现在部门编号":
+                    a = i.getCurDepartmentid();
+                    break;
+                case "部门职位":
+                    a = i.getEmployPosition();
+                    break;
+                case "姓名":
+                    a = i.getEmployeeName();
+                    break;
+                case "性别":
+                    a = i.getEmployeeSex();
+                    break;
+                case "出生日期":
+                    d = i.getEmployeeBirthday();
+                    break;
+                case "电话":
+                    a = i.getEmployeeTel();
+                    break;
+                case "地址":
+                    a = i.getEmployeeAdd();
+                    break;
+                case "qq":
+                    a = i.getEmployeeQq();
+                    break;
+                case "邮箱":
+                    a = i.getEmployeeMail();
+                    break;
+                case "身份证号":
+                    a = i.getEmployeeIdnumber();
+                    break;
+                case "社会保险号":
+                    a = i.getEmployeeSsnumber();
+                    break;
+                case "工资":
+                    l = i.getEmployeeSalary();
+                    break;
+                case "入公司时间":
+                    d = i.getEmployeeEtime();
+                    break;
+                case "离开公司时间":
+                    d = i.getEmployeeLtime();
+                    break;
+                default:
+                    break;
+            }
+            if (!a.equals("")){
+                if(!a.equals(value)) {
+                    it.remove();
+                }
+            } else if (!d.equals(null)){
+                if (!d.equals(value)){
+                    it.remove();
+                }
+            } else if (l != 0){
+                if (l != Integer.parseInt(value)){
+                    it.remove();
+                }
+            }
+        }
+        model.addAttribute("list", list);
+        return "employee";
+    }
+
     @RequestMapping("toInsertEmployee")
     public String toInsertEmployee(){
         return "insertEmployee";
--- src/main/webapp/WEB-INF/jsp/employee.jsp
+++ src/main/webapp/WEB-INF/jsp/employee.jsp
@@ -943,8 +943,23 @@
                         </div>
                         <div class="row">
                             <div class="col-md-4 column">
-                                <a class="btn btn-primary" href="${path}/Employee/toInsertEmployee">新增</a>
+                                <a class="btn btn-primary" href="${path}/Employee/toInsertEmployee">新增</a><br><br>
                             </div>
+                        </div>
+                        <div>
+                            <form action="" name="userForm">
+                                属性：<input type="text" name="employeeId"><br><br>
+                                取值：<input type="text" name="curDepartmentid"><br><br>
+                                <input type="button" value="查询" onclick="searchEmployee()">
+                            </form>
+                            <script type="text/javascript">
+                                function searchEmployee() {
+                                    var form = document.forms[1];
+                                    form.action = "<%=appPath%>/Employee/searchEmployee";
+                                    form.method = "post";
+                                    form.submit();
+                                }
+                            </script>
                         </div>
                         <div id="grid-pager"></div>
                         <!-- PAGE CONTENT ENDS -->
